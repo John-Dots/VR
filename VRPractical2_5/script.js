40bc6d9d791c9f9085058b83105ca6c3493bdb0b
John-Dots <johnh174@nycstudents.net>
2025-11-15 03:06:31 +0000

    update

--- VRPractical2_5/script.js
+++ VRPractical2_5/script.js
@@ -1,98 +1,79 @@
-/* Note
-   Feel free to use classes from past classwork and practical activities.  You may also create new classes.  
-   If you wish to use objects from Unit 1, you can use cloneNode( ) to duplicate them.  As an added bonus you 
-   can also incorporate classes that have animations integrated into them. 
-*/
-
-/* Challenge 1
-   Add appropriate classes to use as objects in your maze.  Choose characters to represent these objects and 
-   position them in the maze.   In Challenge 3 and 4, you will generate the maze along with any other object 
-   you chose to put in the maze.  Get Creative!
-*/
+// Maze Legend: X = wall, - = open, T = treasure, P = trap, S = start, E = exit
 let maze = [
   "XXXXXXXXXXXXXXXXXXXX",
-  "XS-------X--------EX",
+  "XS-------X-----E--XX",
   "X-XXX-XX-X-XXXXXX-XX",
-  "X-T--XX--T--XX---T-X",
+  "X-T--XX--T--XX----XX",
   "XXXX-XXXXXX-XXXXXXXX",
-  "X------------------X",
+  "X----------P------XX",
   "XXXXXXXXXXXXXXXXXXXX"
 ];
 
-/* Challenge 2
-   Add appropriate classes to use as objects in your map.  Choose characters to represent these objects and position them on the map.   In Challenge 5 and 6, you will generate the map using the character representation of the objects you chose to place in the world. Get Creative!
-*/class MazeWall {
-  constructor(x, y, z) {
+class MazeWall {
+  constructor(x, z) {
     let el = document.createElement('a-box');
     el.setAttribute('position', `${x} 1 ${z}`);
-    el.setAttribute('depth', 1);
-    el.setAttribute('height', 2);
     el.setAttribute('width', 1);
-    // Optionally use a texture
-    // el.setAttribute('src', '#wallTexture');
+    el.setAttribute('height', 2);
+    el.setAttribute('depth', 1);
     el.setAttribute('color', '#444');
-    scene.appendChild(el);
+    document.querySelector('a-scene').appendChild(el);
   }
 }
 
 class MazeTreasure {
-  constructor(x, y, z) {
+  constructor(x, z) {
     let el = document.createElement('a-sphere');
     el.setAttribute('position', `${x} 1 ${z}`);
-    el.setAttribute('radius', 0.5);
-    el.setAttribute('color', '#FFD700');
-    scene.appendChild(el);
+    el.setAttribute('radius', 0.4);
+    el.setAttribute('color', 'gold');
+    document.querySelector('a-scene').appendChild(el);
   }
 }
 
 class MazeTrap {
-  constructor(x, y, z) {
+  constructor(x, z) {
     let el = document.createElement('a-cone');
     el.setAttribute('position', `${x} 0.5 ${z}`);
-    el.setAttribute('radius-bottom', 0.5);
+    el.setAttribute('radius-bottom', 0.4);
     el.setAttribute('height', 1);
-    el.setAttribute('color', '#FF3333');
-    scene.appendChild(el);
+    el.setAttribute('color', '#f33');
+    document.querySelector('a-scene').appendChild(el);
   }
 }
 
+class MazeMarker {
+  constructor(x, z, type) {
+    let el = document.createElement('a-box');
+    el.setAttribute('position', `${x} 0.5 ${z}`);
+    el.setAttribute('width', 1);
+    el.setAttribute('height', 1);
+    el.setAttribute('depth', 1);
+    el.setAttribute('color', type === "start" ? "#33f" : "#3f3");
+    document.querySelector('a-scene').appendChild(el);
+  }
+}
 
-let scene;
-
-window.addEventListener("DOMContentLoaded",function() {
-  scene = document.querySelector("a-scene");
-  for(let r = 0; r < maze.length; r++){
-    /* Challenge 3
-      Choose a technique to traverse the each character in the string.
-    */ window.addEventListener("DOMContentLoaded", function () {
-  scene = document.querySelector("a-scene");
-  for (let r = 0; r < maze.length; r++) {
+window.addEventListener("DOMContentLoaded", function() {
+  for(let r = 0; r < maze.length; r++) {
     let row = maze[r];
-    for (let c = 0; c < row.length; c++) {
+    for(let c = 0; c < row.length; c++) {
       let char = row[c];
-      let x = c - (row.length / 2);
-      let z = r - (maze.length / 2);
+      let x = c - Math.floor(row.length / 2);
+      let z = r - Math.floor(maze.length / 2);
 
-      switch (char) {
-        case "X":
-          new MazeWall(x, 0, z);
-          break;
-        case "T":
-          new MazeTreasure(x, 0, z);
-          break;
-        case "P":
-          new MazeTrap(x, 0, z);
-          break;
-        // "S", "E", "-" are open spaces or can add entrance/exit markers if desired
+      if (char === "X") {
+        new MazeWall(x, z);
+      } else if (char === "T") {
+        new MazeTreasure(x, z);
+      } else if (char === "P") {
+        new MazeTrap(x, z);
+      } else if (char === "S") {
+        new MazeMarker(x, z, "start");
+      } else if (char === "E") {
+        new MazeMarker(x, z, "exit");
       }
+      // "-" is empty, no action needed
     }
   }
 });
-
-    /* Challenge 4
-       Make an appropriate decision based on the characters you chose to enter 
-       in the maze.  Create an instance of the corresponding object.
-    */
-  }
-
-})
