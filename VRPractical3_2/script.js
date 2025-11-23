6f24f5e432c19a44f188d719c065d4626690c67d
John-Dots <johnh174@nycstudents.net>
2025-11-23 19:59:54 +0000

    update

--- VRPractical3_2/script.js
+++ VRPractical3_2/script.js
@@ -1,16 +1,45 @@
-let scene;
+window.addEventListener("DOMContentLoaded", function () {
+  scene = document.querySelector("a-scene");
 
+  // Register car interaction - moves when clicked
+  AFRAME.registerComponent('drivable-car', {
+    init: function () {
+      this.el.addEventListener('click', () => {
+        this.el.setAttribute('animation', {
+          property: 'position',
+          to: '10 0 0',
+          dur: 1500,
+          easing: 'linear'
+        });
+      });
+    }
+  });
 
-window.addEventListener("DOMContentLoaded",function() {
-  scene = document.querySelector("a-scene")
+  // Register UFO interaction - disappears on mouseover
+  AFRAME.registerComponent('shootable-ufo', {
+    init: function () {
+      this.el.addEventListener('mouseenter', () => {
+        this.el.setAttribute('visible', false);
+      });
+    }
+  });
 
+  // Populate multiple cars and UFOs
+  const car = document.createElement('a-box');
+  car.setAttribute('drivable-car', '');
+  car.setAttribute('position', '0 0 -5');
+  car.setAttribute('color', 'red');
+  scene.appendChild(car);
 
-      
+  const ufo = document.createElement('a-sphere');
+  ufo.setAttribute('shootable-ufo', '');
+  ufo.setAttribute('position', '0 3 -8');
+  ufo.setAttribute('color', 'gray');
+  scene.appendChild(ufo);
+
   loop();
-})
+});
 
-function loop(){
-
-    
+function loop() {
   window.requestAnimationFrame(loop);
 }
